55ea29f2f366268b1c4082bdfc6351e46a5e8ad8
Thomas Weissinger <tweissin@cisco.com>
2017-03-22 22:37:23 -0400

    update the application used to register an application so it's a little more user friendly.  Also update the README for usage instructions.

---
+++ Sample_Swing_UI/register.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CLASSPATH=`ls target/*.jar`
+for i in $DIR/target/dependency/*.jar; do
+    CLASSPATH=$CLASSPATH\:$i
+done
+
+java com.cisco.thunderhead.example.ui.Register
