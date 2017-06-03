2aef9d8fd82e8c3b37b0a398f5a8a3a724bc80a5
Thomas Weissinger <tweissin@cisco.com>
2017-06-02 23:31:11 -0400

    improve functional test

--- Import_Export_Sample_Code/README.md
+++ Import_Export_Sample_Code/README.md
@@ -11,6 +11,22 @@ Consider using the sample codes in Lab mode to test and build custom apps for yo
 
 After compiling with `mvn clean install` you can run `export.sh` to export or `import.sh` to import.
 
+w## Getting Started
+To set up sample code components:
+
+1. Download the Context Service SDK from [Context Service Downloads](https://developer.cisco.com/fileMedia/download/dcf47513-a2cb-407c-b8a5-cc0d8f620405).
+    
+    You receive context-service-sdk-X.X.X.tar.gz where "X.X.X" is the current version of the SDK.
+    
+2. Run `prepareSDK.sh` with parameter \[targz-filename\] 
+    
+    This sets up your connector.properties file.
+    
+3. Run `mvn clean install`.
+4. [Register with Context Service](https://developer.cisco.com/site/context-service/documents/context-service-sdk-guide/index.gsp#register-your-application-with-context-service) to receive the connectionData string. 
+    The connectionData string is required to connect to Context Service.
+5. Save your connectionData string to connectiondata.txt in your project directory.
+
 ## Disclaimer
 This sample code is only a sample and is NOT guaranteed to be bug free and production quality. This is NOT intended to be used in production environment. You must adapt the code to work with your custom application.
 
--- Import_Export_Sample_Code/src/test/java/com/cisco/thunderhead/sample/importexport/FunctionalTest.java
+++ Import_Export_Sample_Code/src/test/java/com/cisco/thunderhead/sample/importexport/FunctionalTest.java
@@ -8,15 +8,22 @@ import com.cisco.thunderhead.util.RFC3339Date;
 import com.cisco.thunderhead.util.SDKUtils;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -31,7 +38,19 @@ public class FunctionalTest {
     public void testExport() throws Exception {
         assumeTrue("must have connectiondata.txt file to test this", Utils.getConnectionData().length() > 0);
         ContextServiceClient contextServiceClient = Utils.initContextServiceClient(Utils.getConnectionData());
-        Customer customer = createCustomerAndWait(contextServiceClient);
+        Map<String,Object> map = new HashMap<String, Object>() {{
+            put("Context_Work_Email", "john.doe@example.com");
+            put("Context_Work_Phone", "555-555-5555");
+            put("Context_First_Name", "John");
+            put("Context_Last_Name", "Doe");
+            put("Context_Street_Address_1", "123 Sesame Street");
+            put("Context_City", "Detroit");
+            put("Context_State", "MI");
+            put("Context_Country", "US");
+            put("Context_ZIP", "90210");
+        }};
+
+        Customer customer = createCustomerAndWait(contextServiceClient, map);
         Path dir = Files.createTempDirectory("export");
 
         try {
@@ -47,31 +66,49 @@ public class FunctionalTest {
             FileReader fr = new FileReader(file);
             JsonArray customers = new Gson().fromJson(fr, JsonArray.class);
             assertEquals("wrong number of customers", 1, customers.size());
+
+            validateCustomers(customers,map);
+
         } finally {
             // cleanup
             contextServiceClient.delete(customer);
-//            Files.delete(dir);
+            Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+        }
+    }
+
+    private void validateCustomers(JsonArray customers, Map<String, Object> map) {
+        JsonArray dataElementsList = customers.get(0).getAsJsonObject().getAsJsonArray("dataElements");
+        assertEquals(map.size(), dataElementsList.size());
+        for (int i=0; i<dataElementsList.size(); i++) {
+            JsonObject jsonObject = dataElementsList.get(i).getAsJsonObject();
+            for (Map.Entry<String, JsonElement> e : jsonObject.entrySet()) {
+                if (e.getKey().equals("type"))
+                    continue;
+                assertEquals(e.getValue().getAsString(), map.get(e.getKey()).toString());
+            }
+            System.out.println(jsonObject);
         }
     }
 
     /**
      * Creates a customer and waits for it to be searchable.
      */
-    private Customer createCustomerAndWait(ContextServiceClient contextServiceClient) throws InterruptedException {
+    private Customer createCustomerAndWait(ContextServiceClient contextServiceClient, Map<String, Object> map) throws InterruptedException {
         Customer customer = new Customer(
-                DataElementUtils.convertDataMapToSet(
-                        new HashMap<String, Object>() {{
-                            put("Context_Work_Email", "john.doe@example.com");
-                            put("Context_Work_Phone", "555-555-5555");
-                            put("Context_First_Name", "John");
-                            put("Context_Last_Name", "Doe");
-                            put("Context_Street_Address_1", "123 Sesame Street");
-                            put("Context_City", "Detroit");
-                            put("Context_State", "MI");
-                            put("Context_Country", "US");
-                            put("Context_ZIP", "90210");
-                        }}
-                )
+                DataElementUtils.convertDataMapToSet(map)
         );
         customer.setFieldsets(Collections.singletonList("cisco.base.customer"));
         ClientResponse clientResponse = contextServiceClient.create(customer);
