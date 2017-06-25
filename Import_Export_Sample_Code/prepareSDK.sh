f9ad5d5dfad922d9b1a1833b8a8aa61a2f4f49b3
Thomas Weissinger <tweissin@cisco.com>
2017-06-25 11:35:31 -0400

    fix when parameter does not include full path

--- Import_Export_Sample_Code/prepareSDK.sh
+++ Import_Export_Sample_Code/prepareSDK.sh
@@ -8,6 +8,25 @@ fi
 
 TAR_GZ=$1
 
+function abspath {
+    if [[ -d "$1" ]]
+    then
+        pushd "$1" >/dev/null
+        pwd
+        popd >/dev/null
+    elif [[ -e $1 ]]
+    then
+        pushd "$(dirname "$1")" >/dev/null
+        echo "$(pwd)/$(basename "$1")"
+        popd >/dev/null
+    else
+        echo "$1" does not exist! >&2
+        return 127
+    fi
+}
+
+TAR_GZ=`abspath $TAR_GZ`
+
 SDK_VERSION=`echo $TAR_GZ | sed -n 's/.*context-service-sdk-\([0-9]\.[0-9]\.[0-9]\)\.tar\.gz/\1/p'`
 PROJECT_DIR=$PWD
 PROP_FILE="connector.property"
