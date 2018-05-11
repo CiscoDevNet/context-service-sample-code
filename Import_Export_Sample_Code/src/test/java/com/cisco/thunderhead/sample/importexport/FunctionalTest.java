cd370ccc3174e5cc83b3fa0224965723d2256ffe
huiche <huiche@cisco.com>
2018-05-11 08:54:59 -0400

    increased waittime for customer is available for search

--- Import_Export_Sample_Code/src/test/java/com/cisco/thunderhead/sample/importexport/FunctionalTest.java
+++ Import_Export_Sample_Code/src/test/java/com/cisco/thunderhead/sample/importexport/FunctionalTest.java
@@ -203,7 +203,7 @@ public class FunctionalTest {
         String id = SDKUtils.getIdFromResponse(clientResponse);
         LOGGER.info("Created customerId=" + id);
         Utils.waitForSearchable(contextServiceClient, Collections.singletonList(id), ContextObject.class, ContextObject.Types.CUSTOMER);
-        Thread.sleep(10000);
+        Thread.sleep(30000);
         return customer;
     }
 }
