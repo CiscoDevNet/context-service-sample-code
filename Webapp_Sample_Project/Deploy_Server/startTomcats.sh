4af3ce4fff5370b9adfef2c8379daad775e73062
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-16 12:30:48 +0800

    Delete Webapp_Sample_Project directory

--- Webapp_Sample_Project/Deploy_Server/startTomcats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export CATALINA_HOME=`pwd`/shared-tomcat
-export CATALINA_BASE=`pwd`/rest-tomcat
-$CATALINA_HOME/bin/startup.sh
-
-export CATALINA_BASE=`pwd`/management-tomcat
-$CATALINA_HOME/bin/startup.sh
-
--- Webapp_Sample_Project/Deploy_Server/stopTomcats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CATALINA_HOME=`pwd`/shared-tomcat
-export CATALINA_BASE=`pwd`/rest-tomcat
-$CATALINA_HOME/bin/shutdown.sh
-
-export CATALINA_BASE=`pwd`/management-tomcat
-$CATALINA_HOME/bin/shutdown.sh
